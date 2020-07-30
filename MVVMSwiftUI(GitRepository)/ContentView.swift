c996c71e700e2f444f06a4b8c9a6621a8335172f
Hoorad Ramezani <hooradramezani@gmail.com>
2020-07-30 11:07:02 +0430

    Initial Commit

---
+++ MVVMSwiftUI(GitRepository)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMSwiftUI(GitRepository)
+//
+//  Created by Hoorad on 7/30/20.
+//  Copyright © 2020 Hoorad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
